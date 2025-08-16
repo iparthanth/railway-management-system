0ea5ed38e31fba22efa2768458e319a912bfe7c5
Parthanth <parthanth@gmail.com>
2025-08-16 22:24:38 +0600

    Add more sample coaches with various types and configurations

--- database/seeders/CoachSeeder.php
+++ database/seeders/CoachSeeder.php
@@ -13,7 +13,11 @@ class CoachSeeder extends Seeder
             ['train_id' => 1, 'coach_number' => 'C1', 'coach_type' => 'business', 'total_seats' => 24, 'price_multiplier' => 2.5],
             ['train_id' => 1, 'coach_number' => 'C2', 'coach_type' => 'first_class', 'total_seats' => 52, 'price_multiplier' => 1.5],
             ['train_id' => 2, 'coach_number' => 'C1', 'coach_type' => 'business', 'total_seats' => 60, 'price_multiplier' => 1.2],
-            
+            ['train_id' => 2, 'coach_number' => 'C2', 'coach_type' => 'economy', 'total_seats' => 80, 'price_multiplier' => 1.0],
+            ['train_id' => 3, 'coach_number' => 'C1', 'coach_type' => 'business', 'total_seats' => 48, 'price_multiplier' => 2.0],
+            ['train_id' => 3, 'coach_number' => 'C2', 'coach_type' => 'first_class', 'total_seats' => 32, 'price_multiplier' => 1.8],
+            ['train_id' => 4, 'coach_number' => 'C1', 'coach_type' => 'business', 'total_seats' => 60, 'price_multiplier' => 1.2],
+            ['train_id' => 5, 'coach_number' => 'C1', 'coach_type' => 'economy', 'total_seats' => 80, 'price_multiplier' => 1.0],
         ]);
     }
 }
